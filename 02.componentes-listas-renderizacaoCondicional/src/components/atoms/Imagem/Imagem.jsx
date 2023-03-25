4ca73aa09998b53698c8b4fa3206b657f6bc467d
Rosana Rezende <rezende_rosana@hotmail.com>
2023-03-25 10:28:03 -0300

    adiciona imagem

---
+++ 02.componentes-listas-renderizacaoCondicional/src/components/atoms/Imagem/Imagem.jsx
@@ -0,0 +1,11 @@
+import "./Imagem.css";
+
+export default function Imagem(props) {
+  return (
+    <img
+      className="imagem quadrado"
+      src="https://via.placeholder.com/150"
+      alt="Imagem"
+    />
+  );
+}
