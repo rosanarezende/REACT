2badf10f8b1a3a75808f894f578513a0550deeef
Rosana Rezende <rezende_rosana@hotmail.com>
2023-04-08 19:44:18 -0300

    remove arquivos

--- 07.rotas_context/src/components/molecules/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header/Header';
-export { default as Counter } from './Counter/Counter';
-export { default as Product } from './Product/Product';
-export { WelcomeCard, WelcomeCard2, WelcomeCard3, WelcomeCard4, WelcomeCard5 } from './WelcomeCard/WelcomeCard';
-export { WelcomeMessage, WelcomeMessage2, WelcomeMessage3 } from './WelcomeMessage/WelcomeMessage';
--- 07.rotas_context/src/components/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Composition } from './Composition/Composition'
-export { default as Context } from './Context/Context'
-export { default as DetailRoute } from './DetailRoute/DetailRoute'
-export { default as Home } from './Home/Home'
