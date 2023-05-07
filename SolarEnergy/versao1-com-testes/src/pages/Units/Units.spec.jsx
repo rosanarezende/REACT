1097736b67149274db9a9f015e74fc2aa325f0bc
Rosana Rezende <rezende_rosana@hotmail.com>
2023-05-07 16:29:04 -0300

    cria Units testes

---
+++ SolarEnergy/versao1-com-testes/src/pages/Units/Units.spec.jsx
@@ -0,0 +1,33 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+import Units from "./Units";
+
+const renderComponent = () =>
+  render(
+    <BrowserRouter>
+      <Units />
+    </BrowserRouter>
+  );
+
+describe("Units", () => {
+  it("should render UnitList component as default", async () => {
+    renderComponent();
+
+    const title = await screen.findByRole("heading", { name: "Unidades" });
+    expect(title).toBeInTheDocument();
+  });
+
+  it("should render UnitRegister component when click on button", async () => {
+    const { container } = renderComponent();
+    const user = userEvent.setup();
+
+    const button = screen.getByText("Nova Unidade");
+    await user.click(button);
+
+    const unitRegister = await screen.findByRole("heading", {
+      name: "Cadastro de Unidade Gerada",
+    });
+    expect(unitRegister).toBeInTheDocument();
+  });
+});
