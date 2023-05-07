b647cf077fb21a84393090c63ee3cc0b2fefe986
Rosana Rezende <rezende_rosana@hotmail.com>
2023-05-06 21:36:27 -0300

    adiciona scripts de teste

---
+++ SolarEnergy/versao1-com-testes/src/utils/mocks.js
@@ -0,0 +1,16 @@
+export const mockUnitList = [
+  {
+    id: 1,
+    apelido: "Unidade 1",
+    local: "São Paulo",
+    marca: "Marca 1",
+    modelo: "Modelo 1",
+  },
+  {
+    id: 2,
+    apelido: "Unidade 2",
+    local: "São Paulo",
+    marca: "Marca 2",
+    modelo: "Modelo 2",
+  }
+];
